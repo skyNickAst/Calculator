df452d50e50dd23fdccf4fd5714ce078e7c67762
Nikolai Astakhov <skynetprojectone@gmail.com>
2023-01-06 01:39:58 +0300

    Initial Commit

---
+++ My Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Calculator
+//
+//  Created by Nikolai Astakhov on 06.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
