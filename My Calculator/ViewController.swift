009d37c7f8d0ffe85fcf75db1646ba9b422305fb
Nikolai Astakhov <skynetprojectone@gmail.com>
2023-01-06 11:02:18 +0300

    Add "AC", "+/-" and "%" functionality

--- My Calculator/ViewController.swift
+++ My Calculator/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var isFinishedTypingNumber = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        
+        if displayLabel.text == "0," {
+            displayLabel.text = "0"
+        }
+        let number = Double(displayLabel.text!)!
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            }
+        }
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        let numValue = sender.currentTitle
+        
+        if isFinishedTypingNumber && numValue != "." {
+            displayLabel.text = numValue
+            isFinishedTypingNumber = false
+        } else if isFinishedTypingNumber && numValue == "." {
+            displayLabel.text! += numValue!
+            isFinishedTypingNumber = false
+        } else if displayLabel.text!.contains(".") && numValue == "." {
+            return
+        } else if displayLabel.text!.first == "0" && numValue == "." && displayLabel.text!.count == 2 {
+            return
+        } else if displayLabel.text!.first == "0" && numValue == "0" && displayLabel.text!.count == 1 {
+            return
+        } else if displayLabel.text!.first == "0" && numValue != "." && displayLabel.text!.count == 1 {
+            displayLabel.text! = numValue!
+        }
+        else {
+            displayLabel.text! += numValue!
+        }
+    }
+    
 
 }
-
